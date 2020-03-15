a0a1c930fefd5ae744e2e5c10cfe55c4c77b9f43
Abdullah Akram Ansari <abdullaha@nulogy.com>
2020-03-15 10:52:15 -0400

    Alpha Version Ready

--- public/javascript/map.js
+++ public/javascript/map.js
@@ -1,2 +1,60 @@
-var myMap = L.map('mapid').setView([51.505, -0.09], 13);
-L.tileLayer.provider('CartoDB.DarkMatter').addTo(myMap);
+var map = L.map('map').setView([38, -95], 4);
+L.tileLayer.provider('CartoDB.DarkMatterNoLabels').addTo(map);
+
+function onEachFeature(feature, layer) {
+    if (feature.properties && feature.properties.popupContent) {
+        layer.bindPopup(`
+            <h2>${feature.properties.NAME}</h2>
+            <h3 style="margin-bottom: 0.25rem;">Status</h3><span style="text-transform: capitalize">${feature.properties.status}</span>
+            <h3 style="margin: 1rem 0 0.25rem 0;">Senator</h3><span>${feature.properties.popupContent.senator}</span>
+       `);
+    }
+}
+
+const color_picker = status => {
+    switch (status) {
+        case "allowed":
+            return "green";
+        case "changing":
+            return "yellow";
+        default:                // banned
+            return "red";
+    }
+};
+
+const zoomer = () => {
+    if(map.getZoom() === 6) {
+        STATES_LAYER.remove();
+
+        if(COUNTIES_LAYER === null) {
+            COUNTIES_LAYER = L.geoJSON(
+                US_COUNTIES,
+                featureOptions
+            ).addTo(map);
+        } else {
+            COUNTIES_LAYER.addTo(map);
+        }
+    } else if (map.getZoom() === 5) {
+        if(COUNTIES_LAYER !== null) COUNTIES_LAYER.remove();
+        STATES_LAYER.addTo(map);
+    }
+};
+
+const featureOptions = {
+    onEachFeature: onEachFeature,
+    style: feature => {
+        return {
+            color: color_picker(feature.properties.status),
+            weight: 1
+        }
+    }
+};
+
+let STATES_LAYER = L.geoJSON(
+    US_STATES,
+    featureOptions
+).addTo(map);
+
+let COUNTIES_LAYER = null;
+
+map.on('zoomend', zoomer);
---
+++ public/javascript/state-converter.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const fs = require('fs');
+
+const generateRandomStatus = () => {
+    let roll = Math.floor(Math.random() * 30);
+    if(roll > 20) return 'allowed';
+    if(roll > 10) return 'changing';
+    return 'banned'
+};
+
+const generateRandomSenator = () => {
+    let first_name = 'Patrick';
+    let last_name = 'McDouglas';
+    switch(Math.floor(Math.random() * 5)) {
+        case 0:
+            first_name = 'John';
+            break;
+        case 1:
+            first_name = 'Stephen';
+            break;
+        case 2:
+            first_name = 'Richard';
+            break;
+        case 3:
+            first_name = 'Jamar';
+            break;
+    }
+    switch(Math.floor(Math.random() * 5)) {
+        case 0:
+            last_name = 'Nash';
+            break;
+        case 1:
+            last_name = 'Fitzgerald';
+            break;
+        case 2:
+            last_name = 'Liebermann';
+            break;
+        case 3:
+            last_name = 'Roth';
+            break;
+    }
+    return first_name + " " + last_name;
+};
+
+let rawData = fs.readFileSync('../data/states.json');
+let counties = JSON.parse(rawData);
+counties.features.forEach(feature => {
+    feature.properties.status = generateRandomStatus();
+    feature.properties.popupContent = {
+        senator: generateRandomSenator()
+    }
+});
+
+fs.writeFileSync('../data/states-modified.json', JSON.stringify(counties));

187183569b2a704b7ca7756ad91129d21e2481d8
Abdullah Akram Ansari <abdullaha@nulogy.com>
2020-03-15 19:47:28 -0400

    Ready for review

--- public/javascript/map.js
+++ public/javascript/map.js
@@ -1,33 +1,57 @@
-var map = L.map('map').setView([38, -95], 4);
-L.tileLayer.provider('CartoDB.DarkMatter').addTo(map);
+const MAP = L.map('map').setView([38, -95], 4);
+const darkTileLayer = L.tileLayer.provider('CartoDB.DarkMatter').addTo(MAP);
+const lightTileLayer = L.tileLayer.provider('CartoDB.Positron');
+let darkModeOn = true;
+let highlightProvince = true;
+
+document.addEventListener("keydown", evt => {
+    if(evt.key === 'k') {
+        highlightProvince = !highlightProvince;
+    }
+    if(evt.key === 'l') {
+        if (darkModeOn) {
+            darkTileLayer.remove();
+            lightTileLayer.addTo(MAP);
+        } else {
+            lightTileLayer.remove();
+            darkTileLayer.addTo(MAP);
+        }
+        darkModeOn = !darkModeOn;
+    }
+});
 
 const color_picker = status => {
     switch (status) {
         case "allowed":
             return "green";
         case "changing":
-            return "yellow";
+            return "orange";
         default:                // banned
             return "red";
     }
 };
 
 const highlightFeature = event => {
-    // var layer = event.target;
-    //
-    // layer.setStyle({
-    //     weight: 5,
-    //     color
-    //     fillOpacity: 0.7
-    // });
-    //
-    // if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-    //     layer.bringToFront();
-    // }
+    if(!highlightProvince) return;
+
+    var feature = event.target;
+
+    feature.setStyle({
+        weight: 5,
+        color: darkModeOn ? "beige" : "gray",
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        feature.bringToFront();
+    }
 };
 
 const resetHighlight = event => {
+    if(!highlightProvince) return;
 
+    STATES_LAYER.resetStyle(event.target);
+    if(COUNTIES_LAYER !== null) COUNTIES_LAYER.resetStyle(event.target);
 };
 
 const onEachFeature = (feature, layer) => {
@@ -38,30 +62,30 @@ const onEachFeature = (feature, layer) => {
             <h3 style="margin: 1rem 0 0.25rem 0;">Senator</h3><span>${feature.properties.popupContent.senator}</span>
         `;
 
-        layer.bindPopup(feature.properties.COUNTY === undefined ? popupBody : `<h2>${feature.properties.NAME}</h2>`);
+        layer.bindPopup(feature.properties.COUNTY === undefined ? popupBody : `<h3>${feature.properties.NAME}</h3>`);
     }
 
     layer.on({
         mouseover: highlightFeature,
         mouseout: resetHighlight,
-    })
+    });
 };
 
 const zoomer = () => {
-    if(map.getZoom() === 6) {
+    if(MAP.getZoom() === 7) {
         STATES_LAYER.remove();
 
         if(COUNTIES_LAYER === null) {
             COUNTIES_LAYER = L.geoJSON(
                 US_COUNTIES,
                 featureOptions
-            ).addTo(map);
+            ).addTo(MAP);
         } else {
-            COUNTIES_LAYER.addTo(map);
+            COUNTIES_LAYER.addTo(MAP);
         }
-    } else if (map.getZoom() === 5) {
+    } else if (MAP.getZoom() === 6) {
         if(COUNTIES_LAYER !== null) COUNTIES_LAYER.remove();
-        STATES_LAYER.addTo(map);
+        STATES_LAYER.addTo(MAP);
     }
 };
 
@@ -78,8 +102,22 @@ const featureOptions = {
 let STATES_LAYER = L.geoJSON(
     US_STATES,
     featureOptions
-).addTo(map);
+).addTo(MAP);
 
 let COUNTIES_LAYER = null;
 
-map.on('zoomend', zoomer);
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = () => {
+
+    var div = L.DomUtil.create('div', 'info legend');
+    div.innerHTML += '<i style="background: #FF0000;"></i>Banned<br>';
+    div.innerHTML += '<i style="background: #FFA500;"></i>Changing<br>';
+    div.innerHTML += '<i style="background: #00FF00;"></i>Allowed<br>';
+
+    return div;
+};
+
+legend.addTo(MAP);
+
+MAP.on('zoomend', zoomer);
